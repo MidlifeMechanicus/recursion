e0f29541feb7cfbbb2f64570a221d6b041e4b07b
ElectronDragon <electricdragondevelopment@gmail.com>
2024-06-27 12:44:30 +0100

    Refactor #fibs_rec

--- lib/main.rb
+++ lib/main.rb
@@ -16,17 +16,43 @@ end
 p fibs(8)
 
 def fibs_rec(n)
-  array = []
-  if n <2
-    array[n] = n
-    return n
-  else
-    array[n-1] = (fibs_rec(n-1) + fibs_rec(n-2))
-    return (fibs_rec(n-1) + fibs_rec(n-2))
-  end
-  array
+  # These are the base cases, as they are assigned, not calculated
+  return [] if n == 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+
+  array = fibs_rec(n-1)
+  array << array[-2] + array[-1]
 end
 
 p fibs_rec(8)
-# def merge_sort(array)
+
+# def merge_sort(full_array)
+  # if full_array.length <= 1
+    # return full_array
+  # else
+    # midpoint = full_array.length / 2
+    # left_half = full_array[0, midpoint]
+    # right_half = full_array[midpoint, -1]
+    # merge(merge_sort(left_half), merge_sort(right_half))
+  # end
 # end
+# 
+# def merge (array1, array2)
+  # merged_array = []
+  # until array1.empty? || array2.empty?
+    # if array1[0] < array2[0]
+      # merged_array << array1.shift
+    # else
+      # merged_array << arrray2.shift
+    # end
+  # end
+# end
+
+# This one was tough; I understood the concept, but it did not occur to me to write a separate method for the merging.
+
+a = [3, 2, 1, 13, 8, 5, 0, 1]
+b = [105, 79, 100, 110] 
+
+# p merge_sort(a)
+# p merge_sort(b)
